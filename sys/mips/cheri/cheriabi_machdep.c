9f76f11fb4b8c28d6a8009512c06bd1fa55c4509
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-01-17 13:47:57 +0000

    rtld-cheri-elf: let the kernel load relocbase into $c4
    The kernel has that information easily available whereas it is quite hard
    to do this in the assembly in a way that LLD will accept.
    LLD refuses insert an absolute reloction into PIC binaries which is the
    approach currently used to calculate the image base address. We could
    modify lld to provide a symbol that can be loaded using PIC_LOAD_CODE_PTR
    but as we already have the information available that seems overly
    complicated.

--- sys/mips/cheri/cheriabi_machdep.c
+++ sys/mips/cheri/cheriabi_machdep.c
@@ -872,7 +872,9 @@ cheriabi_exec_setregs(struct thread *td, struct image_params *imgp, u_long stack
 	    (imgp->args->argc + imgp->args->envc + 2) * sizeof(void * __capability);
 	cheri_capability_set(&td->td_frame->c3, CHERI_CAP_USER_DATA_PERMS,
 	    auxv, imgp->auxarg_size * 2 * sizeof(void * __capability), 0);
-
+	/* Load relocbase in $c4 for rtld-cheri-elf */
+	cheri_capability_set(&td->td_frame->c4, CHERI_CAP_USER_DATA_PERMS,
+	    (void *)imgp->reloc_base, CHERI_CAP_USER_DATA_LENGTH, 0);
 	/*
 	 * Restrict the stack capability to the maximum region allowed for
 	 * this process and adjust sp accordingly.
