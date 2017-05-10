800d61a109c91e76d958673e2f50993fd569688f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-05-10 17:55:56 +0000

    Teach another printf to use vaddr_t

--- contrib/curl/lib/mprintf.c
+++ contrib/curl/lib/mprintf.c
@@ -839,7 +839,7 @@ static int dprintf_formatf(
           base = 16;
           digits = (p->flags & FLAGS_UPPER)? upper_digits : lower_digits;
           is_alt = 1;
-          num = (size_t) ptr;
+          num = (vaddr_t) ptr;
           is_neg = 0;
           goto number;
         }
